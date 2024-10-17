2c70cda77ad78b142bb48698259466d068c2d46d
jmoura <julien.moura@actia.fr>
2024-10-17 17:51:23 +0200

    Update script de generation de BD

--- database/create-db.sql
+++ database/create-db.sql
@@ -16,6 +16,7 @@ CREATE DATABASE foodfighter
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 	
+
 -- Table: public.food
 
 -- DROP TABLE IF EXISTS public.food;
@@ -23,10 +24,11 @@ CREATE DATABASE foodfighter
 CREATE TABLE IF NOT EXISTS public.food
 (
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
-    name text COLLATE pg_catalog."default",
-    description text COLLATE pg_catalog."default",
-    lat text COLLATE pg_catalog."default",
-    lon text COLLATE pg_catalog."default",
+    name character varying(255) COLLATE pg_catalog."default",
+    description character varying(255) COLLATE pg_catalog."default",
+    lat double precision,
+    lng double precision,
+    image bytea[],
     CONSTRAINT food_pkey PRIMARY KEY (id)
 )
-	
+
